10051c2310921d5a45f7c88b3b291c310e91c6e0
EliGurarie <eliezg@uw.edu>
2019-03-28 00:31:52 -0400

    factor of two!

--- R/getRhoDelta.R
+++ R/getRhoDelta.R
@@ -2,22 +2,23 @@
 #' 
 #' Functions for converting the concentration parameter \eq{\rho} to the season duration parameter \eq{\delta} and vice versa.  They are: \code{findDelta(rho)} and \code{findDelta(delta)}.  These are not very exciting functions, but making them work correctly involved quite a bit of calculus and algebra!
 #' 
+#' @seealso iwc
 #' 
 #' @examples 
-#' @seealso iwc
 #' findDelta(rho = 0.9); findRho(0.0167)
 #' findDelta(rho = 0.1); findRho(0.218)
-#' rhos <- seq(0, 1, length = 1e3)
+#' 
 #' 
 #' # Plot the relationship
 #' par(mfrow = c(1,2))
+#' rhos <- seq(0, 1, length = 1e3)
 #' plot(rhos, Vectorize(findDelta, vectorize.args = "rho")(rhos), ylab = "deltas", type = "l")
-#' deltas <- seq(0, .25, length = 1e3)
+#' deltas <- seq(0, .5, length = 1e3)
 #' plot(deltas, Vectorize(findRho, vectorize.args = "delta")(deltas), ylab = "rhos", type = "l")
 
 #' @export
 findDelta <- function(rho){
-  ifelse(rho == 0, 0.25, 
+  ifelse(rho == 0, 0.5, 
          ifelse(rho == 1, 0, 
                 uniroot(DeltaToRho, rho = rho, interval = c(0, .5))$root))
 }
@@ -25,13 +26,13 @@ findDelta <- function(rho){
 #' @export
 findRho <- function(delta){
   RhoToDelta <- function(rho, delta) DeltaToRho(delta, rho)
-  ifelse(delta == 0.25, 0, 
+  ifelse(delta == 0.5, 0, 
          ifelse(delta == 0, 1,
                 uniroot(RhoToDelta, delta = delta, interval = c(0+1e-6, 1-1e-6))$root))
 }
 
 #' @export
 DeltaToRho <- function(delta, rho){
-  iwc(t = .5 + delta, mu = .5, rho = rho, tau = 1) - 
-    iwc(t = .5 - delta, mu = .5, rho = rho, tau = 1) - 1/2
+  iwc(t = .5 + delta/2, mu = .5, rho = rho, tau = 1) - 
+    iwc(t = .5 - delta/2, mu = .5, rho = rho, tau = 1) - 1/2
 }
