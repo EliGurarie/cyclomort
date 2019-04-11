61ba3f90a7c69939be4d1be7b5927049385704f4
Pete Thompson <pthompson234@gmail.com>
2019-04-11 09:00:31 -0400

    added plot output to summary.cmfitlist

--- R/summary_cmfit.R
+++ R/summary_cmfit.R
@@ -1,6 +1,7 @@
 #'Provides a short summary of the cmfitlist object, displaying the effectiveness of each model
 #'
 #'@param x a cmfitlist object
+#'@param plotme plots AIC/BIC values from each cmfit object in the list to identify the best model choice
 #'
 #'@return a data frame describing the AIC, log-likelihood, number of parameters and parameter estimates for each model
 #'
@@ -8,16 +9,20 @@
 #'@export
 
 
-summary.cmfitlist = function(x) {
+summary.cmfitlist = function(x, plotme = FALSE) {
   estimates = ldply(x, function(l) cbind(l$n.seasons, rbind(ldply(l$estimates))))
   names(estimates)[1] = "n.seasons"
   estimates$parameter[is.na(estimates$parameter)] = "meanhazard"
   method = attributes(x)$method
   if (method == "AIC") {
     values = ldply(x, AIC) %>% rename(c(V1 = "AIC"))
+    if (plotme) {
+      plot(0:(length(x) - 1), values[,2], xlab = "Number of seasons", ylab = "Akaike Information Criterion (AIC)")
+    }
   } else {
     # if (method == "BIC")
     values = ldply(x, BIC) %>% rename(c(V1 = "BIC"))
+    plot(0:(length(x) - 1), values[,2], xlab = "Number of seasons", ylab = "Bayesian Information Criterion (BIC)")
   }
   list(estimates = estimates, model.accuracy = values)
 }
