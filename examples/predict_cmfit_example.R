91e7505a677953f4c64ecd5fcdfaa3df3c35abf5
Pete Thompson <pthompson234@gmail.com>
2019-05-12 16:54:12 -0400

    added plot.cmfactorfit

--- examples/predict_cmfit_example.R
+++ examples/predict_cmfit_example.R
@@ -2,7 +2,7 @@ T <- simPeriodicMorts(300, period = 1, peaks = c(0.3, 0.8),
                       durations = c(0.15, 0.20), weights = c(3, 2)/5, 
                       meanhazard = 1, plotme = FALSE, max.periods = 6)
 T.fit <- fit_cyclomort(T, n.seasons = 2)
-predictions = predict(T.fit, t = 0.5, CI = TRUE, type = "timetodeath")
+predictions = predict(T.fit, t = 0.5, CI = TRUE, type = "hazard")
 
-# The plotting method uses this function.
-plot(T.fit, CI = TRUE, nreps = 1e3, breaks = 40)
+plot(predictions, fit = "null")
+plot(predictions, fit = "alt")
